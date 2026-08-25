98f0199a371a174df6ed4df96da501e32efb7918
Saendhor <spennisi@login02.leonardo.local>
2026-08-25 16:10:45 +0200

    chore: updated scripts

--- bash_scripts/inference.sh
+++ bash_scripts/inference.sh
@@ -1,5 +1,5 @@
 # Script for inference
-uv run python -m scripts.compress --config_path ./config/crdr.yaml --model_path ./crdr.pth.tar --img_dir dataset/block --save_dir ./results/crdr_q000_b384_mc -q 0.00 -b 3.84 --decompress -d cuda
+uv run python -m scripts.compress --config_path ./config/crdr.yaml --model_path ./crdr.pth.tar --img_dir dataset/src --save_dir ./results/inf -q 0.00 -b 3.84 --decompress -d cuda
 
 # Test command
 # poetry run python scripts/compress.py --config_path ./config/crdr.yaml --model_path ./crdr.pth.tar --img_dir ./demo_images --save_dir ./demo_results/crdr_q000_b384_kodak -q 0.00 -b 3.84 --decompress -d cuda
